c23297f6c42f2f31d018b8386d8bc857bfebfc70
songkongji <seungmin576@gmail.com>
2025-02-10 17:19:50 +0900

    User Entity 이름과 비밀번호에 사이즈 설정

--- src/main/java/com/example/schedule_jpa/entity/User.java
+++ src/main/java/com/example/schedule_jpa/entity/User.java
@@ -2,7 +2,9 @@ package com.example.schedule_jpa.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,17 +18,19 @@ public class User extends BaseEntity{
 
     @Column(nullable = false)
     @Setter
-    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 20)
     private String userName;
 
     @Email
     @Column(unique = true, nullable = false)
     @Setter
-    @NotNull
+    @NotBlank
     private String email;
 
     @Column(nullable = false)
-    @NotNull
+    @NotBlank
+    @Size(min = 4, max = 20)
     private String password;
 
     public User() {
