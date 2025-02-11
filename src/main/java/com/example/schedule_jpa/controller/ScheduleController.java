4ba70d63983bddc203013dad338893df88ef20bb
songkongji <seungmin576@gmail.com>
2025-02-11 20:22:07 +0900

    일정 페이지의 기준 일정 페이지 updateDate 내림차순 기준 정렬 완료

--- src/main/java/com/example/schedule_jpa/controller/ScheduleController.java
+++ src/main/java/com/example/schedule_jpa/controller/ScheduleController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -59,7 +60,7 @@ public class ScheduleController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ){
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "updateDate"));
         Page<PageScheduleResponseDto> schedulePage = scheduleService.getSchedulePage(pageable);
         return new ResponseEntity<>(schedulePage, HttpStatus.OK);
     }
