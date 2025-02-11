f27eb861c34695474579b7e8a80d3d686aedc3dd
songkongji <seungmin576@gmail.com>
2025-02-11 10:15:49 +0900

    Lv7 댓글 entity 생성

---
+++ src/main/java/com/example/schedule_jpa/entity/Comment.java
@@ -0,0 +1,23 @@
+package com.example.schedule_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Comment extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+}
